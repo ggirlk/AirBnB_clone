4fdf8dbccf9052592da485636c1138d81d188f12
ggirlk <ggirlk2@gmail.com>
2020-02-19 22:59:08 +0000

    Console 0.1

--- console.py
+++ console.py
@@ -2,7 +2,9 @@
 """ entry point of the command interpreter """
 import cmd
 import sys
+"""from models import BaseModel
 
+myModels = {"BaseModel": BaseModel}"""
 
 class HBNBCommand(cmd.Cmd):
     """ entry point hbnb Class """
@@ -20,6 +22,21 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ emptyline """
         return False
+
+    def do_create(self, arg):
+        """ create """
+        if not arg:
+            print("** class name missing **")
+        else:
+            try:
+                exec("from models.base_model import %s" % (arg))
+            except:
+                print("** class doesn't exist **")
+                return
+
+    def do_show(self, arg):
+        """  """
+        
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
