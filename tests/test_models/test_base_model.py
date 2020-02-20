0c78658ec4c250be01067d5ccd6ce8f4a23f0e1f
anaruzz <laabidigh@gmail.com>
2020-02-20 11:08:51 +0100

    Test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,22 +1,55 @@
 #!/usr/bin/python3
-
-
-"""BaseModel test suite"""
-import unittest
+"""
+Unittest for BaseModel class.
+"""
 from models.base_model import BaseModel
+import datetime
+import unittest
+import uuid
 
 
-class TestBaseModel(unittest.TestCase):
-    """BaseModel unit tests"""
+class test_file(unittest.TestCase):
+    """
+    Test BaseModel class.
+    """
 
-    def test_base_object_should_be_of_type_BaseModel(self):
-        """tests that instances of BaseModel
-        should be of type BaseModel"""
-        model = BaseModel()
-        self.assertEqual(type(model), BaseModel)
+    def test_bm(self):
+        """
+        Test BaseModel class id values.
+        """
+        bs1 = BaseModel()
+        bs2 = BaseModel()
+        self.assertTrue(isinstance(bs1, BaseModel))
+        self.assertNotEqual(bs1.id, bs2.id)
+        bs1.name = "Holberton"
+        self.assertEqual(bs1.name, "Holberton")
+        self.assertTrue(isinstance(bs1.name, str))
+        bs1.age = 5
+        self.assertEqual(bs1.age, 5)
+        self.assertTrue(isinstance(bs1.age, int))
+        self.assertTrue(isinstance(bs1.created_at, datetime.datetime))
+        self.assertTrue(isinstance(bs1.updated_at, datetime.datetime))
+        self.assertTrue(isinstance(bs1.id, str))
+        self.assertTrue(isinstance(bs1.to_dict(), dict))
+        self.assertTrue(hasattr(bs1, 'id'))
+        self.assertTrue(hasattr(bs1, 'created_at'))
+        self.assertTrue(hasattr(bs1, 'updated_at'))
+        self.assertTrue(hasattr(bs1, 'age'))
+        self.assertTrue(hasattr(bs1, 'name'))
 
-    def test_base_object_has_an_id(self):
-        """tests that BaseModel objects have an
-        id attribute"""
-        model = BaseModel()
-        self.assertEqual(model.id is None, False)
+        dic = bs1.to_dict()
+        self.assertTrue(hasattr(dic, '__class__'))
+
+        d1 = bs1.updated_at
+        bs1.save()
+        d2 = bs1.updated_at
+        self.assertNotEqual(d1, d2)
+
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        for key in my_model_json.keys():
+            self.assertTrue(
+                hasattr(my_model, key)
+                )
